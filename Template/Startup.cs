a23a027f8f2d4a942ad9dc7f8c6c8c52ee9aa16e
nannipamula <nannipamula.raviteja@indiumsoft.com>
2024-09-10 19:14:40 +0530

    First Commit

--- Template/Startup.cs
+++ Template/Startup.cs
@@ -64,7 +64,7 @@ namespace Template
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=File}/{action=Login}/{id?}");
             });
         }
     }
